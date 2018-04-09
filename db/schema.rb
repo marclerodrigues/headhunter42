a5d620b715d6cdff360431587be94440949ffce7
Marco Echevestre <marcoechevestre@Marcos-MacBook-Air.local>
2018-04-09 16:06:46 -0300

    Make the migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405192901) do
+ActiveRecord::Schema.define(version: 20180409173601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180405192901) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
   end
 
 end
