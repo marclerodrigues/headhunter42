5f766a929a09a3d59771ae2bbe4aac62878ddc52
Marco Echevestre <marcoechevestre@Marcos-MacBook-Air.local>
2018-04-09 15:44:23 -0300

    Add the attribute :role to :User
    - Make the migration of :role
    - Add a test to :role
    - Add enumerize to :role in :User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  extend Enumerize
+
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+
+  enumerize :role, in: [:admin, :reviewer, :candidate], default: :candidate
+
 end
