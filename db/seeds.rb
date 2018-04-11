361edc99d3d166f8f7057719f94069cedef833f7
Marco Echevestre <marcoechevestre@Marcos-MacBook-Air.local>
2018-04-11 17:05:05 -0300

    Add role to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 
 User.create(first_name: 'Admin',
             last_name: 'Headhunter',
+            role: 'Admin',
             email: ENV['ADMIN_EMAIL'],
             password: ENV['ADMIN_PASSWORD'])
