1e8cc39ebf502d93657e5346f44be875cbdfc017
Marco Echevestre <marcoechevestre@Marcos-MacBook-Air.local>
2018-04-13 11:23:27 -0300

    Add loaders to dashboards_controller
    - Add loaders from gem cancancan to dashboards_controller admin and
    reviewer

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,9 +1,9 @@
 module Admin
-  class DashboardsController < ::BaseAdminController
+  class DashboardsController < BaseController
     load_and_authorize_resource
 
     def index
     end
-    
+
   end
 end
