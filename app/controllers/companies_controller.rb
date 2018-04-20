f75c1a0de19b38e13d9bd1c49bb6bc8bfb5f498b
Marco Echevestre <marcoechevestre@Marcos-MacBook-Air.local>
2018-04-20 18:36:22 -0300

    Improve the Company controlle code

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,16 +1,23 @@
 class CompaniesController < ApplicationController
-  respont_to: :html
-  
+  before_action: :set_company, only: [:edit, :update]
+
   def update
-    @company = CompanyForm.find(params[:id])
-    @company.update_attributes(params[:company])
+    respond_to do |format|
+      if @company.update_attributes(:company_params)
+        format.html { redirect_to @company, notice: "Company was sucessfully updated" }
+      else
+        format.hmtl { render :edit, notice: "Company couldn't be updated" }
+      end
   end
 
   def edit
+  end
+
+  def set_company
     @company = CompanyForm.find(params[:id])
   end
 
   def company_params
-    params.require(:company).permit(:logo)
+    params.require(:company).permit(:name, :website, :phone_numbers, :address, :logo)
   end
 end
