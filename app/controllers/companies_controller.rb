4db8660c753a1be63dd2a4d9114df62f15ae1021
Marco Echevestre <marcoechevestre@Marcos-MacBook-Air.local>
2018-04-20 17:57:58 -0300

    Fix some mistakes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,6 @@
 class CompaniesController < ApplicationController
-
+  respont_to: :html
+  
   def update
     @company = CompanyForm.find(params[:id])
     @company.update_attributes(params[:company])
@@ -7,5 +8,9 @@ class CompaniesController < ApplicationController
 
   def edit
     @company = CompanyForm.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:logo)
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,4 @@
 class Company < ApplicationRecord
-  respont_to: html
   attachment :logo, type: :image
 
-  def company_params
-    params.require(:company).permit(:logo)
-  end
 end
