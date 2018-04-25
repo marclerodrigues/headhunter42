07b3947a54ea1caa830dcd32d7ab8a10b6be2e00
Marco Echevestre <marcoechevestre@Marcos-MacBook-Air.local>
2018-04-25 17:03:35 -0300

    Incomplete the code
    - Complete the code from CompanyForm that was missing.
    - Add i18n for flash messages used.

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -1,27 +1,30 @@
 module Admin
-    class CompaniesController < ::Admin::BaseController
+  class CompaniesController < ::Admin::BaseController
     before_action :company, only: [:edit, :update]
 
-
     def update
-      if @company.update_attributes(company_params)
-        flash[:notice] = "Company was successfully updated"
-        redirect_to root_path
+      @company = CompanyForm.new(company_params.merge({ company: Company.first }))
+      if @company.update
+        redirect_to root_path, flash: { notice: t(:company_successfully_updated) }
       else
-        flash[:error] = "Error to update the company"
-        render :edit
+        render :edit, flash: { error: t(:error_while_updating_the_company) }
       end
     end
 
     def edit
+      @company = CompanyForm.new
     end
 
-    def company
-      @company ||= Company.first
-    end
+    private
 
     def company_params
-       params.require(:company).permit(:name, :website, :phone_number, :address, :logo)
+      params.require(:company).permit(
+                :name,
+                :website,
+                :phone_number,
+                :address,
+                :logo
+      )
     end
   end
 end
--- app/forms/company_form.rb
+++ app/forms/company_form.rb
@@ -1,10 +1,47 @@
 class CompanyForm
   include ActiveModel::Model
-  attr_accessor( :name, :website, :phone_numbers, :address, :logo)
+
+  def self.model_name
+      ActiveModel::Name.new(self, nil, 'Company')
+  end
+
+  attr_accessor(
+      :name,
+      :website,
+      :phone_numbers,
+      :address,
+      :logo,
+      :company
+  )
 
   validates :name, presence: true
   validates :website, presence: true
   validates :phone_numbers, presence: true
   validates :address, presence: true
   validates :logo, presence: true
+
+
+  def update
+      if valid?
+        update_company
+      else
+          false
+      end
+  end
+
+  private
+
+  def update_company
+    company.update_attributes(company_attributes)
+  end
+
+  def company_attributes
+    {
+      name: name,
+      website: website,
+      phone_number: phone_number,
+      address: address,
+      logo: logo
+  }
+  end
 end
